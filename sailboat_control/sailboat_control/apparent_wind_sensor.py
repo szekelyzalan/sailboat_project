5a79457d9425d38f8b17a0f86d18a883cabb4974
szekelyzalan <szekelyzalan03@gmail.com>
2026-05-25 16:23:28 +0200

    Use published boat velocity instead of odometry

--- sailboat_control/sailboat_control/apparent_wind_sensor.py
+++ sailboat_control/sailboat_control/apparent_wind_sensor.py
@@ -2,8 +2,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64, Float32  # Imported both to be safe
 from sensor_msgs.msg import Imu
-from nav_msgs.msg import Odometry
 import math
+from geometry_msgs.msg import Vector3
 
 class ApparentWindSensor(Node):
     def __init__(self):
@@ -15,11 +15,15 @@ class ApparentWindSensor(Node):
             Float32, '/vrx/debug/wind/direction', self.true_wind_dir_callback, 10)
         self.true_wind_speed_sub = self.create_subscription(
             Float32, '/vrx/debug/wind/speed', self.true_wind_speed_callback, 10)
+        self.velocity_sub = self.create_subscription(
+            Vector3,
+            '/boat/velocity',
+            self.velocity_callback,
+            10
+        )
         
         self.imu_sub = self.create_subscription(
             Imu, '/boat/imu/data', self.imu_callback, 10)
-        self.odom_sub = self.create_subscription(
-            Odometry, '/vrx/debug/trajectory_body', self.odom_callback, 10) 
 
         # --- Publishers ---
         self.apparent_wind_dir_pub = self.create_publisher(Float32, '/sensor/apparent_wind/direction', 10)
@@ -47,19 +51,17 @@ class ApparentWindSensor(Node):
         cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
         self.boat_heading = math.atan2(siny_cosp, cosy_cosp)
 
-    def odom_callback(self, msg):
-        self.boat_vel_x = msg.twist.twist.linear.x
-        self.boat_vel_y = msg.twist.twist.linear.y
+    def velocity_callback(self, msg):
+        self.boat_vel_x = msg.x
+        self.boat_vel_y = msg.y
 
     def compute_apparent_wind(self):
         # Throttled status log to show us what's happening under the hood
-        if self.true_wind_dir is None or self.true_wind_speed is None or self.boat_heading is None:
-            self.get_logger().info(
-                f"Waiting for data... Wind Dir: {'OK' if self.true_wind_dir is not None else 'MISSING'} | "
-                f"Wind Speed: {'OK' if self.true_wind_speed is not None else 'MISSING'} | "
-                f"IMU Heading: {'OK' if self.boat_heading is not None else 'MISSING'}",
-                throttle_duration_sec=2.0
-            )
+        if None in (
+            self.true_wind_dir,
+            self.true_wind_speed,
+            self.boat_heading
+        ):
             return
 
         # 1. Global True Wind Vector (ENU)
